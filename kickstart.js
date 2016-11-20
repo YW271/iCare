0571c8059d4b0007a303c6f8473cae87a46997d4
YW271 <yuhua0.618@gmail.com>
2016-11-20 19:19:06 +0000

    major changes
    sticky navbar; more divs in the main content

---
+++ kickstart.js
@@ -0,0 +1,12 @@
+var  mn = $(".topnav");
+    mns = "topnav-scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
+
